8c4ce111a4030661111e222f2f3f69a9a2ea4416
Ammar Githam <ammargitham786@gmail.com>
2020-12-07 19:37:14 +0900

    finish activity when onbackpressed bug occurs
    Required so the activity finishes when the bug occurs. Current workaround makes the back button unresponsive

--- app/src/main/java/awais/instagrabber/activities/MainActivity.java
+++ app/src/main/java/awais/instagrabber/activities/MainActivity.java
@@ -215,6 +215,7 @@ public class MainActivity extends BaseLanguageActivity implements FragmentManage
                 super.onBackPressed();
             } catch (Exception e) {
                 Log.e(TAG, "onBackPressed: ", e);
+                finish();
             }
         }
     }
