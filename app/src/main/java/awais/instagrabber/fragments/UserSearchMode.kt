64600ceb046a73da4800dd7f5df7ea70eaeb1caf
Ammar Githam <ammargitham786@gmail.com>
2021-07-07 16:29:44 +0900

    Convert UserSearchFragment to kotlin and fix UserSearchMode

--- app/src/main/java/awais/instagrabber/fragments/UserSearchMode.kt
+++ app/src/main/java/awais/instagrabber/fragments/UserSearchMode.kt
@@ -1,6 +1,6 @@
 package awais.instagrabber.fragments
 
-enum class UserSearchMode(name: String) {
+enum class UserSearchMode(val mode: String) {
     USER_SEARCH("user_name"),
     RAVEN("raven"),
     RESHARE("reshare");
